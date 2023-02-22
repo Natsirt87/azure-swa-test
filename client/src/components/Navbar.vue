275f4adb391c187452bac85105407647bba61081
Natsirt87 <tristancommons@outlook.com>
2023-02-22 00:49:21 -0800

    Started creating prototype for research db project

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { reactive } from 'vue';
+import { VaSidebar, VaSidebarItem, VaSidebarItemContent,
+    VaSidebarItemTitle, VaIcon } from 'vuestic-ui';
+
+const items = reactive([
+    {title: "Projects", icon: "view_list", active: true},
+    {title: "Researchers", icon: "person", active: false},
+    {title: "Funding Sources", icon: "account_balance", active: false}
+]);
+</script>
+
+<template>
+    <div class="h-screen">
+        <va-sidebar hoverable minimized-width="64px" color="#FFF" class="shadow-zinc-400 shadow-lg">
+            <va-sidebar-item
+                class="cursor-pointer" 
+                v-for="item in items" 
+                :key="item.title"
+                :active="item.active"
+            >
+
+                <va-sidebar-item-content>
+                    <va-icon :name="item.icon" />
+                    <va-sidebar-item-title>
+                        {{ item.title }}
+                    </va-sidebar-item-title>
+                </va-sidebar-item-content>
+
+            </va-sidebar-item>
+        </va-sidebar>
+    </div>
+</template>
---
+++ client/src/views/ResearchersView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { VaCard, VaCardTitle, VaCardContent } from 'vuestic-ui';
+</script>
+
+<template>
+  <main>
+    <div class="flex justify-center">
+      <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-5 pt-20 px-10 max-w-5xl">
+        
+        <va-card class="flex-initial md:col-span-2 xl:col-span-3">
+          <va-card-title>Title</va-card-title>
+          <va-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </va-card-content>
+        </va-card>
+
+        <va-card class="flex-initial xl:col-span-2">
+          <va-card-title>Title</va-card-title>
+          <va-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </va-card-content>
+        </va-card>
+
+        <va-card class="flex-initial">
+          <va-card-title>Title</va-card-title>
+          <va-card-content>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </va-card-content>
+        </va-card>
+        
+      </div>
+    </div>
+  </main>
+</template>
