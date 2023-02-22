66d6edb494ae4c4ee3df3464a56381c08252ed78
Tristan <tristancommons@outlook.com>
2023-02-22 01:32:24 -0800

    Add sidebar routing functionality

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,12 +1,15 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, computed } from 'vue';
 import { VaSidebar, VaSidebarItem, VaSidebarItemContent,
     VaSidebarItemTitle, VaIcon } from 'vuestic-ui';
+import { RouterLink, useRoute } from 'vue-router';
+
+const route = useRoute();
 
 const items = reactive([
-    {title: "Projects", icon: "view_list", active: true},
-    {title: "Researchers", icon: "person", active: false},
-    {title: "Funding Sources", icon: "account_balance", active: false}
+    {title: "Projects", icon: "view_list", url: "/", active: computed(() => route.name == "projects")},
+    {title: "Researchers", icon: "person", url: "/researchers", active: computed(() => route.name == "researchers")},
+    {title: "Funding Sources", icon: "account_balance", url: "/", active: false}
 ]);
 </script>
 
@@ -20,12 +23,14 @@ const items = reactive([
                 :active="item.active"
             >
 
-                <va-sidebar-item-content>
-                    <va-icon :name="item.icon" />
-                    <va-sidebar-item-title>
-                        {{ item.title }}
-                    </va-sidebar-item-title>
-                </va-sidebar-item-content>
+                <router-link :to="item.url">
+                    <va-sidebar-item-content>
+                        <va-icon :name="item.icon" />
+                        <va-sidebar-item-title>
+                            {{ item.title }}
+                        </va-sidebar-item-title>
+                    </va-sidebar-item-content>
+                </router-link>
 
             </va-sidebar-item>
         </va-sidebar>
